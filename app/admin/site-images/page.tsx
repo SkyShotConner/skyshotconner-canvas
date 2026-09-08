70f9bb7fd9b183cf5a0e47e7d72b26b1bab1c57e
Conner <conneraviation18@gmail.com>
2026-09-08 18:39:21 +0200

    Load admin website image styles

--- app/admin/site-images/page.tsx
+++ app/admin/site-images/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useMemo, useState } from 'react'
 import { createClient } from '@/lib/supabase/client'
 import SiteImageManager from '../SiteImageManager'
+import '../site-images.css'
 
 export default function SiteImagesAdminPage(){
   const supabase = useMemo(() => createClient(), [])!
