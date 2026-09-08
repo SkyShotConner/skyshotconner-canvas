172e5b672905e544a13097bbaf5f9624c07e12b0
Conner <conneraviation18@gmail.com>
2026-09-08 18:39:26 +0200

    Add admin section navigation

---
+++ app/admin/layout.tsx
@@ -0,0 +1,9 @@
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return <>
+    <div className="admin-section-nav">
+      <a href="/admin">Products</a>
+      <a href="/admin/site-images">Website images</a>
+    </div>
+    {children}
+  </>
+}
