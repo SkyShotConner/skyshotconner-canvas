4626b1b3bac556dd96e43a5c3338596baea37069
Conner <conneraviation18@gmail.com>
2026-09-14 11:59:19 +0200

    Add Special Collection and Branding admin navigation

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,7 +5,9 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
   return <>
     <div className="admin-section-nav">
       <a href="/admin">Products</a>
+      <a href="/admin/special-collection">Special Collection</a>
       <a href="/admin/site-images">Website images</a>
+      <a href="/admin/branding">Branding</a>
     </div>
     {children}
   </>
