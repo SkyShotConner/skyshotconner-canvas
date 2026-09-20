1ddaea4384d42a8d2d56c51b2d58b5fd4f1d6f78
Conner <conneraviation18@gmail.com>
2026-09-20 13:20:28 +0200

    Add Orders to admin navigation

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,10 +1,12 @@
 import './admin-nav.css'
 import './site-images.css'
+import './orders/orders.css'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return <>
     <div className="admin-section-nav">
       <a href="/admin">Products</a>
+      <a href="/admin/orders">Orders</a>
       <a href="/admin/special-collection">Special Collection</a>
       <a href="/admin/site-images">Website images</a>
       <a href="/admin/branding">Branding</a>
